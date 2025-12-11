2749f6995484d42612502dd37df893423881e989
rictic <rictic@gmail.com>
2025-12-11 10:15:04 -0800

    Add a hermetic dev shell with nix.

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "TypeScript Go Playground development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            # Node.js runtime
+            nodejs_22
+
+            # pnpm package manager
+            nodePackages.pnpm
+
+          ];
+
+          shellHook = ''
+            echo "TypeScript Go Playground dev shell"
+            echo "Node.js: $(node --version)"
+            echo "pnpm: $(pnpm --version)"
+          '';
+        };
+      }
+    );
+}
